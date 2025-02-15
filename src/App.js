08c0e91b9b9ee659377db22f53c6b9b0ea9c9178
John Figueiredo <johnfigueiredo@Johns-Laptop.local>
2025-02-15 00:21:09 -0800

    Make sure you can't pull to the left on mobile

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
-import GlobalStyle from './styles/GlobalStyle';
+import { createGlobalStyle } from 'styled-components';
 import theme from './styles/theme';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
@@ -8,6 +8,39 @@ import About from './components/About';
 import Partners from './components/Partners';
 import Portfolio from './components/Portfolio';
 import Exits from './components/Exits';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html, body {
+    margin: 0;
+    padding: 0;
+    overflow-x: hidden;
+    position: fixed;
+    position: relative;
+    width: 100%;
+    height: 100%;
+    -webkit-overflow-scrolling: touch;
+    overscroll-behavior: none;
+    touch-action: pan-y pinch-zoom;
+  }
+
+  #root {
+    width: 100%;
+    overflow-x: hidden;
+    position: relative;
+  }
+
+  body {
+    overscroll-behavior-y: none;
+    overscroll-behavior-x: none;
+    overflow-x: hidden !important;
+  }
+`;
 
 function App() {
   return (
